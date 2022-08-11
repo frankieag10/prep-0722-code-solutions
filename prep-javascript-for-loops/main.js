4dc670b5fad9987acb3aed7e434fa3982f618630
frankieag10 <frankieag10@gmail.com>
2022-08-11 00:05:33 +0000

    completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,14 @@
+// create your loops here.
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let f = 0; f < 20; f = f + 2) {
+  console.log(f);
+}
+
+for (let a = 100; a > 0; a--) {
+  const text = 'time till explosion ' + a;
+  console.log(text);
+}
